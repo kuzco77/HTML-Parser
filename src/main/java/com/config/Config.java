cf744620ba70c826d57c07a30b14af757e88daab
Chu Nam Anh <namanhchu2103@gmail.com>
2020-07-27 17:37:09 +0700

    fix photo link http => https

--- src/main/java/com/config/Config.java
+++ src/main/java/com/config/Config.java
@@ -18,11 +18,11 @@ public class Config {
     public static String VNMEDIA_API_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.S";
 
     /*file config*/
-    public static String IMAGE_STORE_PATH = "/Users/chunamanh/Downloads/logo";
+    public static String IMAGE_STORE_PATH = "/Users/ghddev/Downloads/logo";
 //    public static String IMAGE_STORE_PATH = "/u01/website/phetit.vn/public/images";
 
     /*app config*/
-    public static String LOG_PATH = "/Users/chunamanh/Downloads/logs";
+    public static String LOG_PATH = "/Users/ghddev/Downloads/logs";
 //    public static String LOG_PATH = "/u01/crawl/VNMedia/logs";
 
     /*mysql config*/
--- src/main/java/com/model/FileManager.java
+++ src/main/java/com/model/FileManager.java
@@ -2,11 +2,7 @@ package com.model;
 
 import com.logger.Logger;
 import org.apache.commons.io.FileUtils;
-import sun.rmi.runtime.Log;
-
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -15,9 +11,13 @@ public class FileManager {
     private static final int READ_TIMEOUT = 10000;
 
     public static boolean downloadFile(String url, String outputPath) {
+
+        String secureURL = url.replace("http", "https");
+        System.out.println(secureURL);
         try {
+
             FileUtils.copyURLToFile(
-                    new URL(url),
+                    new URL(secureURL),
                     new File(outputPath),
                     CONNECTION_TIMEOUT,
                     READ_TIMEOUT
